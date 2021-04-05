58d18e23cb24804d5a4940d03a5bd16d8b5af96b
Matt Hunt <wmatthunt@gmail.com>
2021-04-05 19:21:43 -0400

    setup folders

---
+++ WhaleBL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhaleBL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
